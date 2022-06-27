6024d525a9986c9a523bfa06b9d2c9defe58047e
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-06-27 17:01:05 +0700

    fix: change some output

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,17 +1,17 @@
 const response = require('../helpers/standardRespond')
 
 exports.getAllUsers = (req, res) => {
-    return response(res, 'Message from standard get respones')
+    return response(res, 'Message from standard get respones users')
 }
 
 exports.postAllUsers = (req, res) => {
-    return response(res, 'Message from standard post respones')
+    return response(res, 'Message from standard post respones users')
 }
 
 exports.putAllUsers = (req, res) => {
-    return response(res, 'Message from standard put respones')
+    return response(res, 'Message from standard put respones users')
 }
 
 exports.deleteAllUsers = (req, res) => {
-    return response(res, 'Message from standard delete respones')
+    return response(res, 'Message from standard delete respones users')
 }
