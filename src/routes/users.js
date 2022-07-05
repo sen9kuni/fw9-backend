9398ee8cb8e3f73f9ee3a9c5a310288539968fce
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-07-05 13:23:05 +0700

    fix: add sanitasi

--- src/routes/users.js
+++ src/routes/users.js
@@ -11,6 +11,8 @@ const bcrypt = require('bcrypt');
 const createUserValidator = [
   body('email')
     .isEmail().withMessage('Email format invalid'),
+  body('pin').isNumeric().withMessage('pin must number'),
+  body('pin').isLength({min: 6, max: 6}).withMessage('Pin length must be 6'),
   body('username')
     .isLength({min: 4}).withMessage('Username length minimal 4 character'),
   body('password')
@@ -23,10 +25,12 @@ const createUserValidator = [
 
 const editUserValidator = [
   body('email').isEmail().withMessage('Email format invalid'),
+  body('pin').isNumeric().withMessage('pin must number'),
+  body('pin').isLength({min: 6, max: 6}).withMessage('Pin length must be 6'),
   body('username').isLength({min: 4}).withMessage('Username length minimal 4 character'),
   body('password')
     .isLength({min: 8}).withMessage('Password length minimal 8 character')
-    .customSanitizer(async (val) =>{
+    .customSanitizer(async   (val) =>{
       const hash = await bcrypt.hash(val, 10);
       return hash;
     })
