156c5a76ba0dc4570c845caf5b6d2de6f299b73e
prayogo Santoso <prayogo.santoso681@gmail.com>
2022-09-16 14:57:03 +0700

    update: readme

--- src/routes/authenticated.js
+++ src/routes/authenticated.js
@@ -7,15 +7,12 @@ const rule = require('./validator/authenticated');
 
 // GET
 authenticated.get('/profile', authMiddelware, authenticatedController.profile);
-authenticated.patch('/deletePicture', authMiddelware, authenticatedController.deletePicture);
 authenticated.get('/joinUserAndProfile', authMiddelware, authenticatedController.joinUserAndProfile);
 authenticated.get('/joinTransactions', authMiddelware, authenticatedController.joinHistoryTransactions);
 authenticated.get('/joinTransactionsJoin', authMiddelware, authenticatedController.joinHistoryTransactionsMk2);
 authenticated.get('/joinTNotificationJoin', authMiddelware, authenticatedController.joinHistoryNotif);
 authenticated.get('/getAllNotif', authMiddelware, authenticatedController.getAllNotifs);
 authenticated.get('/countNotifications', authMiddelware, authenticatedController.countNotifications);
-authenticated.patch('/readNotification/:id', authMiddelware, authenticatedController.readNotification);
-authenticated.patch('/readAllNotifs', authMiddelware, authenticatedController.readAllNotif);
 // not work yet
 authenticated.get('/historyTransactions', authMiddelware, authenticatedController.historyTransactions);
 // not work yet
@@ -33,6 +30,9 @@ authenticated.post('/transfer', authMiddelware, rule.transfer, authenticatedCont
 authenticated.post('/topup', authMiddelware, rule.transfer, authenticatedController.topUp);
 
 // PATCH
+authenticated.patch('/deletePicture', authMiddelware, authenticatedController.deletePicture);
+authenticated.patch('/readNotification/:id', authMiddelware, authenticatedController.readNotification);
+authenticated.patch('/readAllNotifs', authMiddelware, authenticatedController.readAllNotif);
 authenticated.patch('/profile', authMiddelware, uploadProfile, ...profileValidatorRules, authenticatedController.updateProfile);
 authenticated.patch('/profileName', authMiddelware, ...profileValidatorRules, authenticatedController.updateProfileName);
 authenticated.patch('/changePassword', authMiddelware, rule.changePassword, authenticatedController.changePasswordTest);
